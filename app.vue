222da63fec7b0dab0b9b87621cfa798f8839bc66
muneo992 <info@gloriatrading.autos>
2025-05-21 18:15:56 +0900

    初期設定

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup>
+</script>
+
+<style>
+/* 必要に応じてスタイルを記述 */
+</style>
+
+  <div>
+    <NuxtRouteAnnouncer />
+    <We are Gloria Trading />
+  </div>
+</template>
