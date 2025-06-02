63773468de43e84b61a8fa5110ec35256ac4b08b
muneo992 <info@gloriatrading.autos>
2025-06-02 20:57:12 +0900

    初期設定

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,15 @@
-export default async function ({ redirect }) {
-  const auth0 = await import('~/plugins/auth0');
-  const user = await auth0.getUser();
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const { $auth0 } = useNuxtApp();
 
-  if (!user) {
-    return redirect('/'); // ログインしていない場合はホームページにリダイレクト
+  // ユーザーが認証済みか確認
+  const isAuthenticated = await $auth0.isAuthenticated();
+  if (!isAuthenticated) {
+    // 未認証の場合、GitHubログイン画面へリダイレクト
+    return $auth0.loginWithRedirect({
+      authorizationParams: {
+        connection: 'github', // GitHub接続を指定
+        appState: { targetUrl: to.fullPath }, // ログイン後にリダイレクトするURL
+      },
+    });
   }
-}
+});
